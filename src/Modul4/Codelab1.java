3adc5fdb2936a10f4d33b8a0f1f7448a06ecf20d
Afif Amhar <afifattamimi@gmail.com>
2024-11-13 19:03:05 +0700

    Codelab modul 4

--- src/Modul4/Codelab1.java
+++ src/Modul4/Codelab1.java
@@ -1,6 +1,6 @@
 package Modul4;
 import java.util.Scanner;
-public class Main {
+public class Codelab1 {
     public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
